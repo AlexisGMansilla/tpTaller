8978d72f5d1d1e677699b24315a406c30b353bec
AlexisGMansilla <alexisgmansilla@gmail.com>
2024-09-19 18:43:30 -0300

    urls

--- SysGames/games/forms.py
+++ SysGames/games/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import games
+from .models import Videojuego
 
 class VideojuegoForm(forms.ModelForm):
     class Meta:
-        model = games
+        model = Videojuego
         fields = ['titulo', 'plataforma', 'genero', 'stock']
