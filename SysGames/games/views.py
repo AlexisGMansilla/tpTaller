8f053476a2b7ac9eba52c25c9c6158ae8531ece8
AlexisGMansilla <alexisgmansilla@gmail.com>
2024-09-19 18:22:58 -0300

    changes alexis

--- SysGames/games/views.py
+++ SysGames/games/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import VideojuegoForm
 
-# Create your views here.
+# Vista para registrar nuevos videojuegos
+def registrar_videojuego(request):
+    if request.method == 'POST':
+        form = VideojuegoForm(request.POST)  # Recibir los datos del formulario
+        if form.is_valid():
+            form.save()  # Guardar el nuevo videojuego en la base de datos
+            return redirect('lista_videojuegos')  # Redirigir a la lista de videojuegos después de guardar
+    else:
+        form = VideojuegoForm()  # Formulario vacío si se accede por GET
+    
+    return render(request, 'registrar_videojuego.html', {'form': form})
