7efd5fed621f1a909ea91cd9ee8492dd41337fcc
linuxx456 <enriqueroman457@gmail.com>
2024-09-20 00:07:23 -0300

    punto5 y6

--- SysGames/games/urls.py
+++ SysGames/games/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('registrar/', views.registrar_videojuego, name='registrar_videojuego'),
+    path('videojuegos/', views.listar_videojuegos, name='listar_videojuegos'),
+    path('finalizar_alquiler/<int:alquiler_id>/', views.finalizar_alquiler, name='finalizar_alquiler'),
 ]
--- SysGames/games/views.py
+++ SysGames/games/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import VideojuegoForm
+from .models import Videojuego
+from django.shortcuts import redirect, get_object_or_404
+from .models import Alquiler, Videojuego
+from django.utils import timezone
 
 def registrar_videojuego(request):
     if request.method == 'POST':
@@ -11,3 +15,28 @@ def registrar_videojuego(request):
         form = VideojuegoForm() 
     
     return render(request, 'registrar_videojuego.html', {'form': form})
+
+def listar_videojuegos(request):
+    plataforma_id = request.GET.get('plataforma', None)
+    genero_id = request.GET.get('genero', None)
+
+    if plataforma_id:
+        videojuegos = Videojuego.objects.filter(plataforma__id=plataforma_id)
+    elif genero_id:
+        videojuegos = Videojuego.objects.filter(genero__id=genero_id)
+    else:
+        videojuegos = Videojuego.objects.all()
+
+    return render(request, 'lista_videojuegos.html', {'videojuegos': videojuegos})
+
+def finalizar_alquiler(request, alquiler_id):
+    alquiler = get_object_or_404(Alquiler, id=alquiler_id)
+    if alquiler.fecha_devolucion is None:  # Solo si no se ha devuelto aún
+        alquiler.fecha_devolucion = timezone.now()
+        alquiler.save()
+
+        # Actualizar el stock del videojuego
+        alquiler.videojuego.stock += 1
+        alquiler.videojuego.save()
+
+    return redirect('lista_alquileres')
